65b9f01678065008bd7c9561aafd342eac136323
Darya <dashkabulycheva@gmail.com>
2025-08-19 19:47:50 +0300

    Fix basetest

--- src/test/java/tests/ui/BaseTest.java
+++ src/test/java/tests/ui/BaseTest.java
@@ -67,6 +67,7 @@ public class BaseTest {
             options.addArguments("--disable-popup-blocking");
             options.addArguments("--disable-infobars");
             options.addArguments("--headless");
+            options.addArguments("--window-size=1920,1080");
             driver = new ChromeDriver(options);
         } else if (browser.equalsIgnoreCase("edge")) {
             EdgeOptions options = new EdgeOptions();
